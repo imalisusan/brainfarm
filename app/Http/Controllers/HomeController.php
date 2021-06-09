15d2ca3f2f462ba70c5d36c03ddcde0719198ff6
Imali Susan <susanimali52@gmail.com>
2021-06-08 22:19:02 -0700

    Remove teams from  navbar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use Dnsimmons\OpenWeather\OpenWeather;
 
@@ -10,8 +11,17 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $weather = new OpenWeather();
-        $current = $weather->getCurrentWeatherByPostal('02111');
-        print_r($current);
+        $current = $weather->getCurrentWeatherByCityName('Nairobi');
+
+        $weather = NULL;
+        $weather['condition'] = Arr::get($current, 'condition.name');
+        $weather['condition_desc'] = Arr::get($current, 'condition.desc');
+        $weather['condition_icon'] = Arr::get($current, 'condition.icon');
+        $weather['temperature'] = Arr::get($current, 'forecast.temp');
+        $weather['pressure'] = Arr::get($current, 'forecast.pressure');
+        $weather['humidity'] = Arr::get($current, 'forecast.humidity');
+        dd($weather);
+
         return view('dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/',[HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home',[HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
