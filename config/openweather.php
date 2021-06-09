af35af21ced5f35dec299520be326b3643d87074
Imali Susan <susanimali52@gmail.com>
2021-06-08 17:53:27 -0700

    Install openweather API

---
+++ config/openweather.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'api_key' 				=> '2d27619283879f46cf347130cfba77e4',
+    'api_endpoint_current'  => 'https://api.openweathermap.org/data/2.5/weather?',
+    'api_endpoint_forecast' => 'https://api.openweathermap.org/data/2.5/forecast?',
+    'api_endpoint_onecall'  => 'https://api.openweathermap.org/data/2.5/onecall?',
+    'api_endpoint_history'  => 'https://api.openweathermap.org/data/2.5/onecall/timemachine?',
+    'api_endpoint_icons'    => 'https://openweathermap.org/img/w/',
+    'api_lang' 				=> 'en',
+    'format_date'           => 'm/d/Y',
+    'format_time'           => 'h:i A',
+    'format_day'            => 'l'
+];
+
+?>
