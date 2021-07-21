fd15561f6febc6085f379012cbce2cac329c30b7
Imali Susan <susanimali52@gmail.com>
2021-07-21 13:50:33 +0300

    Add application logo

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img src="{{ asset('images\logo.svg') }}" class="block h-9 w-auto" />
+                     <x-jet-authentication-card-logo class="block h-9 w-auto" />
                     </a>
                 </div>
 
