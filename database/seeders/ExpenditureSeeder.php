aee64c0e81aacec31f9392bc9eedcd8b26b9fcc8
Imali Susan <susanimali52@gmail.com>
2021-08-11 13:13:30 +0300

    Add expenditure and income seeders

--- database/seeders/ExpenditureSeeder.php
+++ database/seeders/ExpenditureSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Expenditure;
 use Illuminate\Database\Seeder;
 
 class ExpenditureSeeder extends Seeder
@@ -13,6 +14,41 @@ class ExpenditureSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $expenditures = [
+            [
+                'type' => 'Manure',
+                'date' => '2021-07-31',
+                'amount' => '19000', 
+                'description' => 'Bought Manure for the farm', 
+                'farmer_id' => '1',
+            ],
+
+            [
+                'type' => 'Manure',
+                'date' => '2021-07-31',
+                'amount' => '20000', 
+                'description' => 'Bought Manure for the farm', 
+                'farmer_id' => '1',
+            ],
+
+            [
+                'type' => 'Plough',
+                'date' => '2021-07-31',
+                'amount' => '19000', 
+                'description' => 'Bought a plough', 
+                'farmer_id' => '1',
+            ],
+        ];
+
+        foreach ($expenditures as $expenditure) {
+            $expenditure = Expenditure::create([
+                   'type' => $expenditure['type'],
+                   'date' => $expenditure['date'],
+                   'amount' => $expenditure['amount'],
+                   'description' => $expenditure['description'],
+                   'farmer_id' => $expenditure['farmer_id'],
+                 ]);
+        }
     }
+    
 }
--- database/seeders/IncomeSeeder.php
+++ database/seeders/IncomeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Income;
 use Illuminate\Database\Seeder;
 
 class IncomeSeeder extends Seeder
@@ -13,6 +14,31 @@ class IncomeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $incomes = [
+            [
+                'date' => '2021-07-31',
+                'amount' => '100000', 
+                'description' => 'Received cash from the government', 
+                'farmer_id' => '1',
+            ],
+
+            [
+                'date' => '2021-07-31',
+                'amount' => '6000', 
+                'description' => 'Received cash from the government', 
+                'farmer_id' => '1',
+            ],
+
+         
+        ];
+
+        foreach ($incomes as $income) {
+            $income = Income::create([
+                   'date' => $income['date'],
+                   'amount' => $income['amount'],
+                   'description' => $income['description'],
+                   'farmer_id' => $income['farmer_id'],
+                 ]);
+        }
     }
 }
